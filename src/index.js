afb5fd44e3f647ba06d23a3acd9e655d58abefbc
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-03-20 23:17:41 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector('#create-task-form');
+  taskForm.addEventListener('submit', handleSubmit);
+  
+  function handleSubmit(e) {
+	e.preventDefault();
+	const formData = new FormData(taskForm);
+	const task = formData.get('new-task-description');
+	addToList(task);
+	taskForm.reset();
+  }
+  
+  function addToList(task) {
+	  const ul = document.querySelector('#tasks');
+	  const li = document.createElement('li');
+	  const deleteButton = addDeleteButton();
+	  deleteButton.textContent = "	x";
+	  li.textContent = task;
+	  li.appendChild(deleteButton);
+	  ul.appendChild(li);
+  }
+  
+  function addDeleteButton() {
+	  const deleteButton = document.createElement('button');
+	  deleteButton.textContent = "	x";
+	  deleteButton.addEventListener('click', (e) => {
+		  const parentElement = e.target.parentElement;
+		  parentElement.remove();
+	  })
+	  return deleteButton;
+  }
+  
 });
